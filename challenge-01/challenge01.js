8e4f873473e88bc6a42091145d7cfef7e14c1b45
Roney Cacau <roneycacau@gmail.com>
2019-12-10 13:37:42 -0300

    resolvido desafio01

---
+++ challenge-01/challenge01.js
@@ -0,0 +1,17 @@
+let myvar;
+myvar = 10;
+soma = 15+8;
+console.log(soma);
+soma++;
+console.log(soma);
+soma *=3;
+console.log(soma);
+let souNinja = true;
+let comida = ["arroz","feijão","ovo"];
+console.log(comida[1]);
+soma===myvar;
+myvar>=soma;
+function divisao(num1, num2){
+    return num1/num2;
+}
+divisao(10,2);
